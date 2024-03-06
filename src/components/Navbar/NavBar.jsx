49b6f3bf390cddacd6916484f40f0f59b30f91cf
cswebdev <csnider91@live.com>
2024-03-05 21:18:58 -0500

    initial styling of navbar

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import "./NavBar.css";
 
 const NavBar = () => {
-   return <div>navbar works</div>;
+   return (
+      <div className="nav-wrapper">
+         <div className="nav-content">
+            <div className="logo-wrapper">
+               <div className="logo"></div>
+            </div>
+            <ul>
+               <li>
+                  <a href="#">Link 1</a>
+               </li>
+               <li>
+                  <a href="#">Link 2</a>
+               </li>
+               <li>
+                  <a href="#">Link 3</a>
+               </li>
+            </ul>
+         </div>
+      </div>
+   );
 };
 
 export default NavBar;

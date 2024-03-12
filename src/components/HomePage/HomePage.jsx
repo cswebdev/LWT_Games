5b40c143bfa6f76f65eb8c67d0cf2938a4e47ba8
cswebdev <csnider91@live.com>
2024-03-11 21:19:05 -0400

    update homepage to follow correct layout

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -2,22 +2,29 @@ import React from "react";
 import "./HomePage.css";
 const HomePage = () => {
    return (
-      <div className="Container">
-         <div className="right-panel">New York Event</div>
-         <div className="middle-panel">
-            <h1>LWT GAMES</h1>
-            <p>
-               Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima,
-               aliquam! Architecto eaque animi ab quisquam unde impedit illo quo
-               voluptatem, placeat officia, ex perspiciatis quos iste quis
-               praesentium ipsum iure.
-            </p>
-            <button type="button" class="btn btn-primary">
-               Play Bingo
-            </button>
+      <>
+         <div className="Container">
+            <section className="container-header">
+               <div className="right-panel">New York Event</div>
+               <div className="middle-panel">
+                  <h1>LWT GAMES</h1>
+                  <p>
+                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                     Minima, aliquam! Architecto eaque animi ab quisquam unde
+                     impedit illo quo voluptatem, placeat officia, ex
+                     perspiciatis quos iste quis praesentium ipsum iure.
+                  </p>
+                  <button type="button" class="btn btn-primary">
+                     Play Bingo
+                  </button>
+               </div>
+               <div className="left-panel">Virtual Event</div>
+            </section>
          </div>
-         <div className="left-panel">Virtual Event</div>
-      </div>
+         <section className="container-body">
+            <h3>Why </h3>
+         </section>
+      </>
    );
 };
 

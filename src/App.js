262063dc6d69b348331599abf5adbdcff2e2f058
cswebdev <csnider91@live.com>
2024-04-08 13:23:24 -0400

    initial commit for footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import NavBar from "./components/Navbar/NavBar";
 import AboutPage from "./components/AboutPage/AboutPage";
 import FAQ from "./components/FAQ/FAQ";
 import GetInvolvedPage from "./components/GetInvolvedPage/GetInvolvedPage";
+import Footer from "./components/Footer/Footer";
 
 function App() {
    return (
@@ -17,6 +18,7 @@ function App() {
             <Route path="/home" element={<HomePage />} />
             <Route path="/get-involved" element={<GetInvolvedPage />} />
          </Routes>
+         <Footer />
       </div>
    );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+   return <div> footer works</div>;
+};
+
+export default Footer;

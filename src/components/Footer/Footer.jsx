00637f5cbd38ec7f7d61f1ea912fe58ebf3576b9
cswebdev <csnider91@live.com>
2024-04-09 21:48:00 -0400

    updating footer content

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,37 @@ import React from "react";
 import "./Footer.css";
 
 const Footer = () => {
-   return <div> footer works</div>;
+   return (
+      <div className="Footer">
+         <div className="row footer-row">
+            <div className="col-4 col-logo">
+               <div className="logo-placeholder"></div>
+               <p>Tegan's House</p>
+               <p>39 LWT Games Drive</p>
+               <p>Portland, Oregon, USA </p>
+            </div>
+            <div className="col-2">
+               <h2>Explore</h2>
+               <ul>
+                  <li className="footer-item">Home</li>
+                  <li className="footer-item">About Us</li>
+                  <li className="footer-item">Contact US</li>
+               </ul>
+            </div>
+            <div className="col-2">
+               <h2>Follow</h2>
+               <ul>
+                  <li className="footer-item">Instagram</li>
+                  <li className="footer-item">Twitter</li>
+                  <li className="footer-item">Linkedin</li>
+               </ul>
+            </div>
+            <div className="col-4">
+               <button className="btn btn-primary">Play Bingo</button>
+            </div>
+         </div>
+      </div>
+   );
 };
 
 export default Footer;

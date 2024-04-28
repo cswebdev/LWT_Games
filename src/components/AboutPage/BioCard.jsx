40c021ac3ea91d549521c5fad3b67667a78aed60
Juliane Cassidy <julianecassidy@gmail.com>
2024-04-28 16:11:23 -0700

    style bio card and list

--- src/components/AboutPage/BioCard.jsx
+++ src/components/AboutPage/BioCard.jsx
@@ -18,32 +18,39 @@ const BioCard = ({ bio }) => {
     return (
         <>
             {bio instanceof Bio
-                ? <div className="BioCard">
-                    <div className="BioCard-graphics-purple">
+                ? <div className={`BioCard-${bio.color}`}>
+                    <div className="BioCard-graphics">
                         <img
                             className="BioCard-headshot img-fluid"
-                            style={{ borderColor: "#7030A0" }}
-                            src={bio.photo}
+                            style={{
+                                borderColor: bio.color === "purple"
+                                ? "#7030A0"
+                                : "#FFA629"
+                            }}                            src={bio.photo}
                             alt={bio.name}
                         />
                         {bio.drawing &&
                             <img
                                 className="BioCard-drawing img-fluid"
-                                style={{ borderColor: "#7030A0" }}
+                                style={{
+                                    borderColor: bio.color === "purple"
+                                    ? "#7030A0"
+                                    : "#FFA629"
+                                }}
                                 src={bio.drawing}
                                 alt={bio.name}
                             />}
-                        <div className="BioCard-arrow-purple">
+                        <div className={`BioCard-arrow-${bio.color}`}>
                             <ArrowRightShort size={36} />
                         </div>
                     </div>
 
                     <div className="BioCard-name">{bio.name}</div>
-                    <div className="BioCard-pronouns">
-                        {bio.pronouns}
-                    </div>
                     <div className="BioCard-role">
                         {bio.role}
+                    </div>
+                    <div className="BioCard-pronouns">
+                        {bio.pronouns}
                     </div>
                 </div>
             : <img className="BioCard-blanks" src={bio.image} alt={bio.alt} />

7f66a5f1db792eeba3639d4132bc0c46f572bc1f
cswebdev <csnider91@live.com>
2024-04-29 18:42:15 -0400

    commenting out unused import

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./HomePage.css";
 import homepageGraphicOne from "../../assets/left_mainpage_image.png";
-import homepageGraphicTwo from "../../assets/right_mainpage_image.png";
+//import homepageGraphicTwo from "../../assets/right_mainpage_image.png";
 import imageAssets from "../../utils/Utils";
 
 const HomePage = () => {

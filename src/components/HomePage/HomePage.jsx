5ca6373dccad0387e909972aa642fd68bb011ed0
cswebdev <csnider91@live.com>
2024-05-09 22:04:55 -0400

    updating spelling

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -22,7 +22,7 @@ const HomePage = () => {
                   <div className="Header-text">
                      <p>
                         <span className="attn-text">
-                           [ Innerconnected Colective ]
+                           [ Interconnected Collective ]
                         </span>
                      </p>
                      <p>Creating community and building </p>

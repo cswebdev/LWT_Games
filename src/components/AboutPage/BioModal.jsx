95b79208c4f15c19626d6488c13a013bb167af6d
Juliane Cassidy <julianecassidy@gmail.com>
2024-05-21 20:06:44 -0700

    add correct carets for read mores

--- src/components/AboutPage/BioModal.jsx
+++ src/components/AboutPage/BioModal.jsx
@@ -91,16 +91,21 @@ const BioModal = ({ bio, ...props }) => {
             {bio.otherSocial && <div className="btn btn-primary BioModal-social-button"><a href={bio.otherSocial}>Social Media</a></div>}
           </div>
           <div className="BioModal-qanda">
-              <div className="BioModal-reasonForJoining">
-                <span className="BioModal-question"><span className="BioModal-q">Q.</span> Why did you join the group?</span><br></br>
-                <span><span className="BioModal-a">A:</span> {bio.reasonForJoining}</span>
-              </div>
-              {!readMoreShow && <div className="BioModal-readmore" onClick={handleShow}>Read More <CaretDown size={36} /></div>}
-              {readMoreShow && <div className="BioModal-gainedFromInCo">
-                <span className="BioModal-question"><span className="BioModal-q">Q.</span> What have you gained from InCo?</span><br></br>
-                <span><span className="BioModal-a">A:</span> {bio.gainedFromInCo}</span>
-              </div>}
+            <div className="BioModal-reasonForJoining">
+              <span className="BioModal-question"><span className="BioModal-q">Q.</span> Why did you join the group?</span><br></br>
+              <span><span className="BioModal-a">A:</span> {bio.reasonForJoining}</span>
             </div>
+            {!readMoreShow && <div className="BioModal-readmore" onClick={handleShow}>
+              Read More
+              <svg className="AboutPage-caretdown" width="40px" height="40px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M16.5303 8.96967C16.8232 9.26256 16.8232 9.73744 16.5303 10.0303L12.5303 14.0303C12.2374 14.3232 11.7626 14.3232 11.4697 14.0303L7.46967 10.0303C7.17678 9.73744 7.17678 9.26256 7.46967 8.96967C7.76256 8.67678 8.23744 8.67678 8.53033 8.96967L12 12.4393L15.4697 8.96967C15.7626 8.67678 16.2374 8.67678 16.5303 8.96967Z" fill="#000000" />
+              </svg>
+            </div>}
+            {readMoreShow && <div className="BioModal-gainedFromInCo">
+              <span className="BioModal-question"><span className="BioModal-q">Q.</span> What have you gained from InCo?</span><br></br>
+              <span><span className="BioModal-a">A:</span> {bio.gainedFromInCo}</span>
+            </div>}
+          </div>
         </div>
       </Modal.Body>
     </Modal>

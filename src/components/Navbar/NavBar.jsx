954ce781bfeff452251cbae66e8eff5f1fd68038
cswebdev <csnider91@live.com>
2024-05-31 10:08:41 -0400

    major updates to redesign page and mobile nav menu

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -6,60 +6,62 @@ import imageAssets from "../../utils/Utils";
 
 const NavBar = () => {
    const [openMenu, setOpenMenu] = useState(false);
+   const [showMenuBtn, setShowMenuBtn] = useState(true);
 
    const toggleMenu = () => {
       setOpenMenu(!openMenu);
+      setShowMenuBtn(!showMenuBtn);
+   };
+
+   const closeMobileNav = () => {
+      setOpenMenu(false);
+      setShowMenuBtn(true);
    };
 
    return (
       <>
-         <MobileNav isOpen={openMenu} toggleMenu={toggleMenu} />
-         <nav className="navbar navbar-expand-lg">
+         <MobileNav isOpen={openMenu} closeMenu={closeMobileNav} />
+         <nav className="navbar nav-wrapper">
             <div className="container">
-               <Link to="/home" className="navbar-brand">
-                  <img
-                     src={imageAssets.logo}
-                     alt="InCO logo"
-                     className="logo-img"
-                  />
-               </Link>
-               <button
-                  className="navbar-toggler"
-                  type="button"
-                  onClick={toggleMenu}
-               >
-                  <span className="navbar-toggler-icon"></span>
-               </button>
-               <div
-                  className={`collapse navbar-collapse ${
-                     openMenu ? "show" : ""
-                  }`}
-               >
-                  <ul className="navbar-nav ml-auto">
-                     <li className="nav-item">
-                        <a href="/about" className="nav-link">
+               <div className="d-flex align-items-center justify-content-between position-relative">
+                  <Link to="/home" className="navbar-brand logo-wrapper">
+                     <img
+                        src={imageAssets.logo}
+                        alt="InCO logo"
+                        className="logo-img m-0 p-0"
+                     />
+                  </Link>
+                  {showMenuBtn && (
+                     <button className="menu-btn" onClick={toggleMenu}>
+                        <span className={"material-symbols-outlined m-0 p-0"}>
+                           menu
+                        </span>
+                     </button>
+                  )}
+               </div>
+               <div className="nav-content">
+                  <ul className="navbar-nav">
+                     <li className="">
+                        <a href="/about" className="menu-item nav-link">
                            ABOUT US
                         </a>
                      </li>
-                     <li className="nav-item">
-                        <Link to="/faq" className="nav-link">
+                     <li className="">
+                        <Link to="/faq" className="menu-item nav-link">
                            FAQ
                         </Link>
                      </li>
-                     <li className="nav-item">
-                        <Link to="/GET INVOLVED" className="nav-link">
+                     <li className="">
+                        <Link to="/GET INVOLVED" className="menu-item nav-link">
                            GET INVOLVED
                         </Link>
                      </li>
-                     <li className="nav-item">
-                        <a
-                           href="https://laneecho.github.io/LWT-bingo/"
-                           className="btn btn-info nav-play-btn"
-                        >
-                           PLAY BINGO
-                        </a>
-                     </li>
                   </ul>
+                  <button type="button" className="btn-info nav-play-btn">
+                     <a href="https://laneecho.github.io/LWT-bingo/">
+                        PLAY BINGO
+                     </a>
+                  </button>
                </div>
             </div>
          </nav>

df24d4ca36a24a05f363e19061bc98fd505777e5
Juliane Cassidy <julianecassidy@gmail.com>
2024-06-07 12:25:19 -0700

    add mobile styling for footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,12 +4,13 @@ import imageAssets from "../../utils/Utils";
 
 const Footer = () => {
    return (
+      <>
       <div className="Footer">
          <div className="row footer-row">
             <div className="logo-frame">
                <img src={imageAssets.logo} alt="" className="footer-logo" />
             </div>
-            <div className="col-logo">
+            <div className="col-newsletter">
                <h3>Join our newsletter!</h3>
                <p>
                   Stay up to date with all things InCo!
@@ -26,7 +27,7 @@ const Footer = () => {
             </div>
             <div className="col col-menu">
                <h3 className="footer-title">Explore</h3>
-               <ul>
+               <ul className="footer-items">
                   <li className="footer-item"><a href="/about">About Us</a></li>
                   <li className="footer-item"><a href="/faq">FAQ</a></li>
                   <li className="footer-item"> <a href="/get-involved">Join Us</a></li>
@@ -48,6 +49,51 @@ const Footer = () => {
             </div>
          </div>
       </div>
+      <div className="Footer-mobile">
+         <div className="row footer-row">
+            <div className="logo-frame">
+               <img src={imageAssets.logo} alt="" className="footer-logo" />
+            </div>
+            <div className="col-newsletter">
+               <h3>Join our newsletter!</h3>
+               <p>
+                  Stay up to date with all things InCo!
+               </p>
+               <input placeholder="Enter Email Address"></input>
+               <button type="button" className="btn btn-info subscribe">
+                  SUBSCRIBE
+               </button>
+            </div>
+            <div className="col Loading-left">
+               <div className="Loading">
+                  <div className="Loading-progress"></div>
+               </div>
+            </div>
+            <div className="col col-menu">
+               <h3 className="footer-title">Explore</h3>
+               <ul className="footer-items">
+                  <li className="footer-item"><a href="/about">About Us</a></li>
+                  <li className="footer-item"><a href="/faq">FAQ</a></li>
+                  <li className="footer-item"> <a href="/get-involved">Join Us</a></li>
+                  <li className="footer-item"><a href="/bingo-link">Play Bingo</a></li>
+               </ul>
+            </div>
+            <div className="col">
+               <div className="Loading">
+                  <div className="Loading-progress"></div>
+               </div>
+            </div>
+            <div className="col col-follow">
+               <h3 className="footer-title">Follow</h3>
+               <div className="share-logos">
+                  <a href="https://www.instagram.com/interconnectedco/"><img className="share-logo" src={imageAssets.instagram} alt="instagram logo" /></a>
+                  <a href="https://www.linkedin.com/company/interconnected-collective/"><img className="share-logo" src={imageAssets.linkedin} alt="linkedin logo" /></a>
+                  <a href="https://www.facebook.com/profile.php?id=61558814939983"><img className="share-logo" src={imageAssets.facebook} alt="facebook logo" /></a>
+               </div>
+            </div>
+         </div>
+      </div>
+      </>
    );
 };
 

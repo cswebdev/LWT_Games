ac0f1a01fa6f97009c90db058dd8141588b92486
Juliane Cassidy <julianecassidy@gmail.com>
2024-06-10 13:52:16 -0700

    add username policy

--- src/components/TermsPages/TermsOfServicePage.jsx
+++ src/components/TermsPages/TermsOfServicePage.jsx
@@ -57,6 +57,79 @@ const TermsOfServicePage = () => {
                     <p>These terms are governed by and construed in accordance with the laws of United States.</p>
                 </li>
             </ol>
+            <h2>Username Policy</h2>
+            <p>
+                A username is the unique name used to identify oneself on InCo’s Leaderboard. We have a few rules on what can and can't be included in usernames. Please see our full policy guidelines below for all the details, including examples of the types of usernames that are and are not allowed.
+            </p>
+            <p>
+                When registering for an account, choose a username. Usernames cannot contain offensive language, third-party trademarks, email or web addresses, or the words "InCo" or “LWT”.
+            </p>
+            <h3>What can usernames include?</h3>
+            <p>
+                Usernames can include letters, numbers, and some symbols. Usernames must be between 5 and 20 characters, and cannot include spaces.
+            </p>
+            <h3>How do I report an inappropriate username?</h3>
+            <p>
+                To report inappropriate usernames, email <a href="mailto:intercoll.team@gmail.com">intercoll.team@gmail.com</a> - make sure your email includes “Inappropriate Username” in the subject and provide the inappropriate username in the email body. InCo has a number of systems in place to identify usernames that violate our policies, and we appreciate community safety. If we detect a name that violates our policies, we'll take steps to address and prevent it.
+            </p>
+            <h3>Full Policy</h3>
+            <p>
+                Each username must be unique. Duplicate usernames are not allowed. Usernames must be between 5 and 20 characters long.
+            </p>
+            <h4>
+                Usernames <b><u>can</u></b> include:
+            </h4>
+            <ul>
+                <li>Letters (a-z, A-Z)</li>
+                <li>Numbers (0-9)</li>
+                <li>Underscores (_)</li>
+            </ul>
+            <h4>
+                Usernames <b><u>cannot</u></b> include:
+            </h4>
+            <ul>
+                <li>Spaces</li>
+                <li>Special characters (e.g., !, @, #, $, %, ^, &, *)</li>
+            </ul>
+            <h4>Inappropriate Usernames</h4>
+            <p>Usernames must not contain or reference:</p>
+            <ul>
+                <li>Offensive, obscene, and/or inappropriate language</li>
+                <li>Drugs (prescription, recreational, or illegal) and/or drug abuse </li>
+                <li>Pornography, sexually explicit, and/or sexually suggestive content or behavior</li>
+                <li>Impersonation of others including but not limited to public figures and/or organizations</li>
+                <li>Elements that imply an email address or web address (for example, xyz.com), including but not limited to .com, .net, .org, .edu or any variation of such (for example, _com or -com)</li>
+                <li>Phone numbers (numeric or alphanumeric)</li>
+                <li>An underscore (_), hyphen (-), or period (.) at the beginning of a username (for example, -NopeUsername)</li>
+                <li>The word InCo (Note: only InCo personal can use the word InCo in their usernames)</li>
+                <li>The same username as another user or similar to an InCo name</li>
+                <li><b>Personal information</b> such as, but not limited to, full names, addresses, phone numbers, or email addresses to protect user privacy</li>
+                <li><b>Respect all cultures, races, disabilities, genders, sexes, nationalities, gender identities, sexual orientations, etc.</b> and should not contain language or references that could be considered slurs, insensitive or offensive</li>
+                <ul>
+                    <li><b>Hate speech will not be permitted.</b> Usernames cannot include nor promote, perpetuate, or glorify  hate speech, violence, discrimination, and/or illegal activities. <b>Find a short list of unpermitted username subjects below:</b></li>
+                    <li>Slurs or epithets of any kind</li>
+                    <li>Racist, anti-Semitic, or otherwise demeaning portrayals, for example through names of caricatures or other exaggerated features, including names of figurines, cartoons, historical advertisements, and golliwogs</li>
+                    <li>Any username associated with the Confederate battle flag and related items with its image</li>
+                    <li>Any username associated with the Historical Holocaust-related and Nazi-related items, including reproductions</li>
+                    <li>Any username associated with the anti-Semititism</li>
+                    <li>Any username associated with the Nazi propaganda</li>
+                    <li>Any username associated with the support of, membership in, or funding of a terrorist organization</li>
+                    <li>Breaking the Law, including Terrorism and Child Exploitation</li>
+                </ul>
+            </ul>
+            <h4>Enforcement</h4>
+            <p>Accounts found in violation of this policy may be subject to:</p>
+            <ul>
+                <li>Username change requests</li>
+                <li>Temporary suspension</li>
+                <li>Permanent suspension, in cases of repeat or severe violations</li>
+            </ul>
+            <h5>Appeals</h5>
+            <p>Users may appeal any enforcement actions by contacting our support team (<a href="mailto:intercoll.team@gmail.com">intercoll.team@gmail.com</a>). Appeals will be reviewed on a case-by-case basis.</p>
+            <h3>Review and Updates</h3>
+            <p>
+                This policy will be reviewed annually and updated as necessary to ensure it remains relevant and effective.
+            </p>
         </div>
     );
 };
